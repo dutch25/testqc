54b02f6186f6118ea4fc8aaf9e58e190aa907056
dutch25 <haminhduccp@gmail.com>
2025-03-25 10:03:19 +0700

    fc

--- app/src/main/java/com/example/testqc/MainActivity.java
+++ app/src/main/java/com/example/testqc/MainActivity.java
@@ -1,73 +1,73 @@
-package com.example.testqc;
+    package com.example.testqc;
 
-import android.os.Bundle;
-import android.view.MotionEvent;
-import android.widget.ImageView;
-import android.widget.ViewFlipper;
+    import android.os.Bundle;
+    import android.view.MotionEvent;
+    import android.widget.ImageView;
+    import android.widget.ViewFlipper;
 
-import androidx.activity.EdgeToEdge;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+    import androidx.activity.EdgeToEdge;
+    import androidx.appcompat.app.AppCompatActivity;
+    import androidx.core.graphics.Insets;
+    import androidx.core.view.ViewCompat;
+    import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
+    public class MainActivity extends AppCompatActivity {
 
-    private ViewFlipper viewFlipper;
-    private float initialX;
-    private ImageView[] dots;
-    private int currentBanner = 0;
-    private int totalBanners = 3;
+        private ViewFlipper viewFlipper;
+        private float initialX;
+        private ImageView[] dots;
+        private int currentBanner = 0;
+        private int totalBanners = 3;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        @Override
+        protected void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.activity_main);
 
-        viewFlipper = findViewById(R.id.viewFlipper);
+            viewFlipper = findViewById(R.id.viewFlipper);
 
-        dots = new ImageView[]{
-                findViewById(R.id.dot1),
-                findViewById(R.id.dot2),
-                findViewById(R.id.dot3)
-        };
+            dots = new ImageView[]{
+                    findViewById(R.id.dot1),
+                    findViewById(R.id.dot2),
+                    findViewById(R.id.dot3)
+            };
 
-        updateDots(0); // Set first dot as active
-    }
-
-    @Override
-    public boolean onTouchEvent(MotionEvent event) {
-        switch (event.getAction()) {
-            case MotionEvent.ACTION_DOWN:
-                initialX = event.getX();
-                return true;
-
-            case MotionEvent.ACTION_UP:
-                float finalX = event.getX();
-
-                if (initialX > finalX) {
-                    // Swipe Left
-                    if (currentBanner < totalBanners - 1) {
-                        viewFlipper.showNext();
-                        currentBanner++;
-                    }
-                } else if (initialX < finalX) {
-                    // Swipe Right
-                    if (currentBanner > 0) {
-                        viewFlipper.showPrevious();
-                        currentBanner--;
-                    }
-                }
-
-                updateDots(currentBanner);
-                return true;
+            updateDots(0); // Set first dot as active
         }
-        return super.onTouchEvent(event);
-    }
 
-    private void updateDots(int position) {
-        for (int i = 0; i < dots.length; i++) {
-            dots[i].setImageResource(i == position ? R.drawable.dot_active : R.drawable.dot_inactive);
+        @Override
+        public boolean onTouchEvent(MotionEvent event) {
+            switch (event.getAction()) {
+                case MotionEvent.ACTION_DOWN:
+                    initialX = event.getX();
+                    return true;
+
+                case MotionEvent.ACTION_UP:
+                    float finalX = event.getX();
+
+                    if (initialX > finalX) {
+                        // Swipe Left
+                        if (currentBanner < totalBanners - 1) {
+                            viewFlipper.showNext();
+                            currentBanner++;
+                        }
+                    } else if (initialX < finalX) {
+                        // Swipe Right
+                        if (currentBanner > 0) {
+                            viewFlipper.showPrevious();
+                            currentBanner--;
+                        }
+                    }
+
+                    updateDots(currentBanner);
+                    return true;
+            }
+            return super.onTouchEvent(event);
+        }
+
+        private void updateDots(int position) {
+            for (int i = 0; i < dots.length; i++) {
+                dots[i].setImageResource(i == position ? R.drawable.dot_active : R.drawable.dot_inactive);
+            }
         }
     }
-}
